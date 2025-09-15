0b309bf8a9b32c18501637e56d17fb7ceb1e8938
tomiconicgit <tomiconicx@icloud.com>
2025-09-15 22:36:28 +0100

    Create Debug.js

---
+++ src/ui/Debug.js
@@ -0,0 +1,15 @@
+// The code for debug.js is identical to your original file.
+// Just place it in /src/ui/Debug.js
+const Debug = {
+    errorLog: [],
+    dom: { /*...*/ },
+    init() { /*...*/ },
+    _createStyles() { /*...*/ },
+    _createDOM() { /*...*/ },
+    _attachEventListeners() { /*...*/ },
+    addError(type, message) { /*...*/ },
+    showModal() { /*...*/ },
+    hideModal() { /*...*/ },
+    _copyErrorsToClipboard() { /*...*/ }
+};
+export default Debug;
