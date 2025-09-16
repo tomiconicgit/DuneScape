472e412cc083d7de261fc7866df0db0129c68fa4
tomiconicgit <tomiconicx@icloud.com>
2025-09-17 00:32:43 +0100

    Update Sky.js

--- src/world/Sky.js
+++ src/world/Sky.js
@@ -3,17 +3,21 @@ import { Sky } from 'three/addons/objects/Sky.js';
 
 export default class GameSky {
     constructor(scene) {
+        // Create the Sky addon instance
         this.sky = new Sky();
         this.sky.scale.setScalar(450000);
         scene.add(this.sky);
 
+        // This vector will hold the sun's direction
         this.sun = new THREE.Vector3();
+        
+        // A shortcut to the sky's shader uniforms
         this.uniforms = this.sky.material.uniforms;
 
-        // Set initial default values
+        // Set some nice initial default values
         this.setParameters({
-            turbidity: 10,
-            rayleigh: 3,
+            turbidity: 5,
+            rayleigh: 0.5,
             mieCoefficient: 0.005,
             mieDirectionalG: 0.7,
             elevation: 2,
@@ -22,34 +26,26 @@ export default class GameSky {
     }
 
     /**
-     * Updates the sky's appearance based on a set of parameters.
+     * Updates the sky's appearance based on parameters.
+     * The most important are elevation (sun height) and azimuth (sun direction).
      * @param {object} params - The parameters to update.
      */
     setParameters(params) {
         if (!params) return;
 
-        const effectController = {
-            turbidity: params.turbidity !== undefined ? params.turbidity : this.uniforms['turbidity'].value,
-            rayleigh: params.rayleigh !== undefined ? params.rayleigh : this.uniforms['rayleigh'].value,
-            mieCoefficient: params.mieCoefficient !== undefined ? params.mieCoefficient : this.uniforms['mieCoefficient'].value,
-            mieDirectionalG: params.mieDirectionalG !== undefined ? params.mieDirectionalG : this.uniforms['mieDirectionalG'].value,
-            elevation: params.elevation !== undefined ? params.elevation : this.elevation,
-            azimuth: params.azimuth !== undefined ? params.azimuth : this.azimuth,
-        };
-
-        this.uniforms['turbidity'].value = effectController.turbidity;
-        this.uniforms['rayleigh'].value = effectController.rayleigh;
-        this.uniforms['mieCoefficient'].value = effectController.mieCoefficient;
-        this.uniforms['mieDirectionalG'].value = effectController.mieDirectionalG;
+        // Update each uniform if a new value is provided
+        if (params.turbidity !== undefined) this.uniforms['turbidity'].value = params.turbidity;
+        if (params.rayleigh !== undefined) this.uniforms['rayleigh'].value = params.rayleigh;
+        if (params.mieCoefficient !== undefined) this.uniforms['mieCoefficient'].value = params.mieCoefficient;
+        if (params.mieDirectionalG !== undefined) this.uniforms['mieDirectionalG'].value = params.mieDirectionalG;
         
-        // Store these for reference
-        this.elevation = effectController.elevation;
-        this.azimuth = effectController.azimuth;
+        // Calculate the sun's 3D position from the elevation and azimuth angles
+        if (params.elevation !== undefined && params.azimuth !== undefined) {
+            const phi = THREE.MathUtils.degToRad(90 - params.elevation);
+            const theta = THREE.MathUtils.degToRad(params.azimuth);
 
-        const phi = THREE.MathUtils.degToRad(90 - this.elevation);
-        const theta = THREE.MathUtils.degToRad(this.azimuth);
-
-        this.sun.setFromSphericalCoords(1, phi, theta);
-        this.uniforms['sunPosition'].value.copy(this.sun);
+            this.sun.setFromSphericalCoords(1, phi, theta);
+            this.uniforms['sunPosition'].value.copy(this.sun);
+        }
     }
 }
