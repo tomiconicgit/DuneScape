eaa3933998217e87a19465e450a6395b5bc33e35
tomiconicgit <tomiconicx@icloud.com>
2025-09-18 00:13:01 +0100

    Update WorldData.js

--- src/world/WorldData.js
+++ src/world/WorldData.js
@@ -1,14 +1,7 @@
 // This file defines the layout of your world.
 
-export const MINE_AREA = {
-    x: 75, y: 75, width: 50, depth: 60, // The entire flat footprint of the mine
-    base_height: 0.0, // The ground level of the mine
-    patches: [
-        { name: 'iron',      x: 68, y: 70, width: 10, depth: 15, height: 0.1 },
-        { name: 'carbon',    x: 82, y: 70, width: 10, depth: 15, height: 0.1 },
-        { name: 'limestone', x: 75, y: 90, width: 15, depth: 10, height: 0.1 }
-    ]
-};
+// Mine moved inland, away from the town
+export const MINE_AREA = { x: 50, y: 60, width: 20, depth: 20, height: 0.1 };
 
 export const TOWN_AREA = { x: 10, y: 0, width: 20, depth: 40, height: 0.1 };
 
