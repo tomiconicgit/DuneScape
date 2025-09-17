7c0ea9dd87ff80bafe6e4621c0137566a4abc5f1
tomiconicgit <tomiconicx@icloud.com>
2025-09-17 23:16:10 +0100

    Update WorldData.js

--- src/world/WorldData.js
+++ src/world/WorldData.js
@@ -6,10 +6,11 @@ export const MINE_AREA = {
     // The levels of the mine, from top to bottom
     levels: [
         { name: 'limestone', depth: 2, radius: 20 },
-        { name: 'carbon', depth: 5, radius: 14 },
-        { name: 'iron', depth: 8, radius: 8 }
+        { name: 'carbon',    depth: 5, radius: 14 },
+        { name: 'iron',      depth: 8, radius: 8  }
     ]
 };
 
 export const TOWN_AREA = { x: 10, y: 0, width: 20, depth: 40, height: 0.1 };
+
 export const OASIS_AREA = { x: -60, y: -80, width: 20, depth: 40, height: -1.0 };
