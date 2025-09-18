c29bf713227403ebefd6dd87c9b5ebe5e33da88c
tomiconicgit <tomiconicx@icloud.com>
2025-09-18 21:20:51 +0100

    Create rockLayout.js

---
+++ src/world/assets/rockLayout.js
@@ -0,0 +1,89 @@
+// File: src/world/assets/rockLayout.js
+
+// This layout is loaded by the game on startup to place rocks permanently.
+export const permanentRocks = [
+    {
+        "type": "Iron Ore",
+        "position": { "x": 64, "y": -10, "z": 28 }
+    },
+    {
+        "type": "Iron Ore",
+        "position": { "x": 58, "y": -10, "z": 28 }
+    },
+    {
+        "type": "Iron Ore",
+        "position": { "x": 65, "y": -10, "z": 32 }
+    },
+    {
+        "type": "Gold Ore",
+        "position": { "x": 70, "y": -10, "z": 28 }
+    },
+    {
+        "type": "Carbon Ore",
+        "position": { "x": 70, "y": -10, "z": 34 }
+    },
+    {
+        "type": "Carbon Ore",
+        "position": { "x": 61, "y": -10, "z": 33 }
+    },
+    {
+        "type": "Carbon Ore",
+        "position": { "x": 69, "y": -10, "z": 38 }
+    },
+    {
+        "type": "Gold Ore",
+        "position": { "x": 53, "y": -10, "z": 28 }
+    },
+    {
+        "type": "Iron Ore",
+        "position": { "x": 47, "y": -10, "z": 28 }
+    },
+    {
+        "type": "Iron Ore",
+        "position": { "x": 55, "y": -10, "z": 35 }
+    },
+    {
+        "type": "Iron Ore",
+        "position": { "x": 50, "y": -10, "z": 32 }
+    },
+    {
+        "type": "Limestone",
+        "position": { "x": 42, "y": -10, "z": 27 }
+    },
+    {
+        "type": "Limestone",
+        "position": { "x": 38, "y": -10, "z": 26 }
+    },
+    {
+        "type": "Limestone",
+        "position": { "x": 40, "y": -10, "z": 33 }
+    },
+    {
+        "type": "Limestone",
+        "position": { "x": 45, "y": -10, "z": 34 }
+    },
+    {
+        "type": "Limestone",
+        "position": { "x": 36, "y": -10, "z": 32 }
+    },
+    {
+        "type": "Limestone",
+        "position": { "x": 31, "y": -10, "z": 26 }
+    },
+    {
+        "type": "Limestone",
+        "position": { "x": 32, "y": -10, "z": 33 }
+    },
+    {
+        "type": "Carbon Ore",
+        "position": { "x": 31, "y": -10, "z": 37 }
+    },
+    {
+        "type": "Carbon Ore",
+        "position": { "x": 36, "y": -10, "z": 38 }
+    },
+    {
+        "type": "Gold Ore",
+        "position": { "x": 59, "y": -10, "z": 39 }
+    }
+];
